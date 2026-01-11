b373d5829e96e9b446b92183fb1158e0842ff5ad
Praise Jacob <iampraisejacob@gmail.com>
2026-01-12 05:21:17 +0530

    add problem 125

---
+++ src/main/java/problems/p125.java
@@ -0,0 +1,32 @@
+package problems;
+
+public class p125 {
+    public static void main(String[] args) {
+        System.out.println(new p125().isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(new p125().isPalindrome("race a car"));
+    }
+
+    public boolean isPalindrome(String s) {
+        char[] arr = s.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            if (!Character.isLetterOrDigit(arr[start])) {
+                start++;
+            } else if (!Character.isLetterOrDigit(arr[end])) {
+                end--;
+            } else {
+                if (Character.toLowerCase(arr[start]) != Character.toLowerCase(arr[end])) {
+                    return false;
+                } else {
+                    start++;
+                    end--;
+                }
+            }
+        }
+
+        return true;
+    }
+}
